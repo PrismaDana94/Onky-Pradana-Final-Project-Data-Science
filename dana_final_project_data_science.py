b7562f909c4433fb11fa66e76952124ecb48d440
Onky Prisma Pradana <freddycull27@gmail.com>
2025-09-15 12:39:48 +0700

    dana_final_project_data_science.py

--- dana_final_project_data_science.py
+++ dana_final_project_data_science.py
@@ -3,25 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-# link raw github
 url = "https://raw.githubusercontent.com/PrismaDana94/Onky-Pradana-Final-Project-Data-Science/main/Dana_Final_Project_Credit_Card_Fraud_detection.csv"
 
-# load data
 df_profit = pd.read_csv(url)
 
-# Tampilkan ke Streamlit
-st.pyplot(fig)
-# tampilkan preview
 st.title("Dana Final Project - Credit Card Fraud Detection")
-st.write("Data Preview", df_profit.head())
+st.write("Preview Data:")
+st.dataframe(df_profit.head())
 
-# Plot Profit Curve
-fig, ax = plt.subplots(figsize=(8, 5))
-ax.plot(df_profit['population_pct'], df_profit['cum_profit'], color="green", label="Profit Curve")
-ax.set_xlabel("Percentage of Population Targeted (%)")
-ax.set_ylabel("Cumulative Profit (£)")
-ax.set_title("Profit Curve for Fraud Detection")
-ax.grid(True)
-ax.legend()
 
 
