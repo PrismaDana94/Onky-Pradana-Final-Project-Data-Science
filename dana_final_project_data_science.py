d0d877c33821aa9cfffa815a998dfe6eb56c7272
Onky Prisma Pradana <freddycull27@gmail.com>
2025-09-15 12:07:07 +0700

    dana_final_project_data_science.py

--- dana_final_project_data_science.py
+++ dana_final_project_data_science.py
@@ -1,6 +1,13 @@
+import streamlit as st
 import pandas as pd
 
+# link raw github
 url = "https://raw.githubusercontent.com/PrismaDana94/Onky-Pradana-Final-Project-Data-Science/main/Dana_Final_Project_Credit_Card_Fraud_detection.csv"
+
+# load data
 df_profit = pd.read_csv(url)
 
+# tampilkan preview
+st.title("Dana Final Project - Credit Card Fraud Detection")
 st.write("Data Preview", df_profit.head())
+
