856458dd646d9d80805aef9ce04431fa488721e6
Onky Prisma Pradana <freddycull27@gmail.com>
2025-09-15 12:10:35 +0700

    dana_final_project_data_science.py

--- dana_final_project_data_science.py
+++ dana_final_project_data_science.py
@@ -9,6 +9,17 @@ url = "https://raw.githubusercontent.com/PrismaDana94/Onky-Pradana-Final-Project
 # load data
 df_profit = pd.read_csv(url)
 
+# Plot Profit Curve
+fig, ax = plt.subplots(figsize=(8, 5))
+ax.plot(df_profit['population_pct'], df_profit['cum_profit'], color="green", label="Profit Curve")
+ax.set_xlabel("Percentage of Population Targeted (%)")
+ax.set_ylabel("Cumulative Profit (£)")
+ax.set_title("Profit Curve for Fraud Detection")
+ax.grid(True)
+ax.legend()
+
+# Tampilkan ke Streamlit
+st.pyplot(fig)
 # tampilkan preview
 st.title("Dana Final Project - Credit Card Fraud Detection")
 st.write("Data Preview", df_profit.head())
